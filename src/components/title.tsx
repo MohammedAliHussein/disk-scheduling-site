64f2ec74bc52931e1b62a3f2c6b01e268fedd08c
Mohammed-Ali Hussein <mohammedalihussein02@gmail.com>
2023-06-22 10:04:36 +0800

    feature/donation: responsive title and disclaimer

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -1,6 +1,6 @@
 const Title = () => {
   return (
-    <p className="antialiased font-extrabold text-white text-4xl text-center">
+    <p className="antialiased font-extrabold text-white text-2xl text-center md:text-4xl">
       Disk Scheduler Visualisation
     </p>
   );
